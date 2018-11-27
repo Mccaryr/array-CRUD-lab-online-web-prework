0b03db3c73a6c8c317ecf96d8f0c733d4a032ce6
Robert McCary <robert.mccary@yahoo.com>
2018-11-27 02:26:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["fire", "earth", "wind", "water",]
+  avatar_elements = ["fire", "earth", "wind", "water",]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  avatar_elements << "metal"
 end
 
 def add_element_to_start_of_array(array, element)
