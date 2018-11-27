fcf826ae077499795dc60b6ef68846075142805b
Robert McCary <robert.mccary@yahoo.com>
2018-11-27 02:31:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  avatar_elements = ["fire", "earth", "wind", "water",]
+  avatar_elements.shift
 end
 
 def retrieve_element_from_index(array, index_number)
