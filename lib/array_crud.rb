15adf2f50ee34d85a55b01ec6d67813c822105d4
Robert McCary <robert.mccary@yahoo.com>
2018-11-27 02:23:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [fire, earth, wind, water]
+  array = [earth, wind, water, fire]
 end
 
 def add_element_to_end_of_array(array, element)
